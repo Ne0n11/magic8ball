6b081903d49247c37a648c6f3a47bd582cecc42c
Arthur Puha <pugaartur@gmail.com>
2021-12-08 14:10:18 +0200

    Magic 8 ball app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,35 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MagicBoll(),
       ),
     );
+
+class MagicBoll extends StatefulWidget {
+  @override
+  _MagicBollState createState() => _MagicBollState();
+}
+
+class _MagicBollState extends State<MagicBoll> {
+  void _genAnswer() {
+    setState(() {
+      num = Random().nextInt(5) + 1;
+    });
+  }
+
+  var num = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: _genAnswer,
+          child: Image.asset('images/ball$num.png'),
+        ),
+      ),
+    );
+  }
+}
